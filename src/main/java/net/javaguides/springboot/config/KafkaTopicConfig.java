70111764bd4f2b2b3797829577158a892439fecc
akshay <akshayuda@cybage.com>
2024-06-07 12:31:26 +0530

    first commit

---
+++ src/main/java/net/javaguides/springboot/config/KafkaTopicConfig.java
@@ -0,0 +1,23 @@
+package net.javaguides.springboot.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+    @Bean
+    public NewTopic javaguidesTopic(){
+        return TopicBuilder.name("myTopic1").build();
+    }
+
+    @Bean
+    public NewTopic javaguidesJsonTopic(){
+        return TopicBuilder.name("myjsonTopic").build();
+    }
+}
